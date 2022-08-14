b545c9b3fe133b07008f013c270a3fa0ad5ed4da
mike <1022986@student.sae.edu.au>
2022-08-14 22:50:32 +0800

    added bad optimisation for loop and cant test (post processing error)

--- Assets/_Project/3-Scripts/3-Minigames/DeathrunMinigame/DeathRunGM.cs
+++ Assets/_Project/3-Scripts/3-Minigames/DeathrunMinigame/DeathRunGM.cs
@@ -80,6 +80,13 @@ public class DeathRunGM : MinigameManager
     {
         if (trapEffects[currentTrap] != null)
         {
+            for (int i = 0; i < timeTillEmitterEnd; i++)
+            {
+                if(i != currentTrap)
+                {
+                    trapEffects[i].SetActive(false);
+                }
+            }
             trapEffects[currentTrap].SetActive(true);
         }
         
