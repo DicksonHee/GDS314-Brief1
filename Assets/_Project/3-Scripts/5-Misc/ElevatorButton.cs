16c78293c2b0fabf66a734f930ea1993d6902f93
DHee-SAE <1023524@student.sae.edu.au>
2022-08-18 18:17:19 +0800

    More sounds added

--- Assets/_Project/3-Scripts/5-Misc/ElevatorButton.cs
+++ Assets/_Project/3-Scripts/5-Misc/ElevatorButton.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ElevatorButton : MonoBehaviour
 {
+    public AudioClip buttonAudio;
+    public AudioClip screenAudio;
+    public AudioClip elevatorAudio;
     public Animator screenAnimator;
     public GameObject buttonLight;
     
@@ -31,9 +34,12 @@ public class ElevatorButton : MonoBehaviour
 
     private IEnumerator StartLoading_CO()
     {
+        Audio_Manager.current.PlaySFX(buttonAudio);
+        Audio_Manager.current.PlaySFX(screenAudio);
         buttonLight.SetActive(false);
         screenAnimator.SetTrigger("Fall");
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(10f);
+        Audio_Manager.current.PlaySFX(elevatorAudio);
         SceneLoad_Manager.LoadSpecificScene(SessionManager.current.GetNextRandomScene());
     }
 }
